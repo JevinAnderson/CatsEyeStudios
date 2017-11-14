b4fbf376a59b147c15b86b4e8c77d3eec2e956cb
Jevin Anderson <jevinanderson@gmail.com>
2017-11-13 17:38:23 -0800

    Updated site font
    From Dosis to Raleway

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,14 @@ import Application from './application';
 
 const node = document.getElementById('application-entry-point');
 render(<Application />, node);
+
+window._printCSS = () => {
+  const nodes = [...document.querySelectorAll('style')];
+  const style = nodes.map(node => node.innerHTML).join('');
+
+  return style;
+};
+
+window._printDOM = () => {
+  return node.innerHTML;
+};
